7e3ba7e23f1045a6001325be8f92e297ff12de9e
bookthief666 <manuel.orrantia@gmail.com>
2026-02-12 01:38:24 -0800

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Liber333 from './liber333.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Liber333 />
-  </React.StrictMode>
-);
