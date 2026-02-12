921e0fabc02b0ab8b2c96ab7c259c87e60796996
bookthief666 <manuel.orrantia@gmail.com>
2026-02-12 01:30:01 -0800

    Fix formatting and syntax in main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Liber333 from './liber333.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Liber333 from './liber333.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Liber333 />
-  </React.StrictMode>,
-)
-
+  </React.StrictMode>
+);
