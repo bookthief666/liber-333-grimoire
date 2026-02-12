e53f82b66cca3ad4608b2d9c32f58b504ba70cce
bookthief666 <manuel.orrantia@gmail.com>
2026-02-12 01:28:33 -0800

    Add main.jsx to render Liber333 component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Liber333 from './liber333.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Liber333 />
+  </React.StrictMode>
+);
